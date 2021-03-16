1e297f104b277c03b98ea43c9efcf49059df8dd7
PetrovSergSerg <petrov.serg.serg@gmail.com>
2021-03-17 01:08:02 +0300

    added config-file reading

--- conftest.py
+++ conftest.py
@@ -1,22 +1,26 @@
 import pytest
+import json
+import os.path
 from fixture.application import Application
-from data.user import User
 
 
 fixture = None
+config = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global config
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    elif not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=base_url)
+    if config is None:
+        conf_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--config"))
+        with open(conf_file_path) as config_file:
+            config = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=config["baseUrl"])
 
-    fixture.session.ensure_login(User.ADMIN)
+    fixture.session.ensure_login(name=config["login"], pwd=config["password"])
 
     return fixture
 
@@ -35,4 +39,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--config", action="store", default="config.json")
--- fixture/session.py
+++ fixture/session.py
@@ -1,26 +1,23 @@
-from data.user import User
-
-
 class SessionHelper:
     def __init__(self, app):
         self.app = app
 
-    def login(self, user: User):
+    def login(self, name:str, pwd:str):
         wd = self.app.wd
         self.app.open_index_page()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(user.login)
+        wd.find_element_by_name("user").send_keys(name)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(user.password)
+        wd.find_element_by_name("pass").send_keys(pwd)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def ensure_login(self, user: User):
+    def ensure_login(self, name: str, pwd: str):
         if self.is_logged_in():
-            if self.is_logged_in_as(user.login):
+            if self.is_logged_in_as(name):
                 return
             else:
                 self.logout()
-        self.login(user)
+        self.login(name, pwd)
 
     def logout(self):
         wd = self.app.wd
